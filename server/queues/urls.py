b51d5692157fe784160a20e1dab07221b2f89064
Phoenix009 <jaideepmore307@gmail.com>
2020-12-16 15:41:01 +0530

    queue view

--- server/queues/urls.py
+++ server/queues/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name='queues'
 urlpatterns = [
     path('', views.index , name = 'index'),
     path('<str:room_name>/', views.room, name = 'room'),
